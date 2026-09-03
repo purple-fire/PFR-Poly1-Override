d42ed58026b8b6229bb9db6bdd90bfed21ca2e92
warhead20071 <picklesrangel@gmail.com>
2026-09-03 13:25:05 -0400

    Update main.cpp
    changed variable name

--- src/main.cpp
+++ src/main.cpp
@@ -69,15 +69,15 @@ void autonomous() {
  * task, not resume it from where it left off.
  */
 void opcontrol() {
-	pros::Controller master(pros::E_CONTROLLER_MASTER);
+	pros::Controller controller(pros::E_CONTROLLER_MASTER);
 	pros::MotorGroup left_mg({-11, -12, 13, -14});    // Creates a motor group with forwards ports 1 & 3 and reversed port 2
 	pros::MotorGroup right_mg({17, 18, -19, 20});  // Creates a motor group with forwards port 5 and reversed ports 4 & 6
 
 
 	while (true) {
 		// Arcade control scheme
-		int dir = master.get_analog(ANALOG_LEFT_Y);    // Gets amount forward/backward from left joystick
-		int turn = -master.get_analog(ANALOG_RIGHT_X); // Gets the turn left/right from right joystick
+		int dir = controller.get_analog(ANALOG_LEFT_Y);    // Gets amount forward/backward from left joystick
+		int turn = -controller.get_analog(ANALOG_RIGHT_X); // Gets the turn left/right from right joystick
 		left_mg.move(dir - turn);                      // Sets left motor voltage
 		right_mg.move(dir + turn);                     // Sets right motor voltage
 		pros::delay(20);							// Run for 20 ms then update
