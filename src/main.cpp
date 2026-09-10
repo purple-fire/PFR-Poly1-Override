7ffe549e5ac80dc9bde3157391b4e1151d036182
warhead20071 <picklesrangel@gmail.com>
2026-09-10 15:18:17 -0400

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -73,8 +73,6 @@ void opcontrol() {
 	Controller controller(E_CONTROLLER_MASTER);
 	MotorGroup left_mg({-11, -12, 13, -14});    // Creates a motor group with forwards ports 1 & 3 and reversed port 2
 	MotorGroup right_mg({17, 18, -19, 20});  // Creates a motor group with forwards port 5 and reversed ports 4 & 6
-	
-	int num1=5;
 
 	while (true) {
 		// Arcade control scheme
